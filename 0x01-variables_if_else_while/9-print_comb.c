6044aad820d5e5dbaeeb94ce40b952f83bdee090
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-17 06:50:13 -0700

    .

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- prints all possible combinations of single-digit numbers
+ *
+ * Return: Alwaysa 0 {Success)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= 9)
+	{
+		putchar(ch);
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
