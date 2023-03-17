a470159668fdbf70a3884c1f2bdae4a2499e5ca0
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-17 05:45:23 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- prints number 0-9
+ * only using putchar and no char type
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
