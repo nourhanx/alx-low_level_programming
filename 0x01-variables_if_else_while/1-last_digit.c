fbf593b2844633c54f45e43b8aa014d8e9bdad20
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-17 04:44:16 -0700

    .

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints last digit of randomly generated number
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int lstdgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	lstdgt = n % 10;
+	if (lstdgt == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, lstdgt);
+	}
+	else if (lstdgt < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstdgt);
+	}
+	else if (lstdgt > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstdgt);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 solutions to C-variables,if else and while tasks
 
-0-positive_or_negative.c  assign a random number to the variable n each time it is executed
+0-  assign a random number to the variable n each time it is executed and print whether -ve, +ve or zero
+1- assign a random number to the variable n each time it is executed and print last digit
