c553a4d93af7c9dcb0dbe96ed80c41eb15125c09
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-17 04:53:16 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- prints lowercase alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		putchar('\n');
+		ch++;
+	}
+}
