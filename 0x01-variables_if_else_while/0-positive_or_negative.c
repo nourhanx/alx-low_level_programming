4300225856a090cf0a9f419df3eec7e61c4ad987
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-17 04:24:33 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n >= 0)
 	{
-		printf("positive");
+		printf("%d is positive", n);
 	}
 	else
 	{
-		printf("negative");
+		printf("%d is negative", n);
 	}
 	return (0);
 }
