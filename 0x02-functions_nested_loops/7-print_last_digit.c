78430606a035dd1cc982269de5ede9bee8e2facf
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-21 16:27:56 -0700

    solution to 7-

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints units of number
+ *
+ * @r: integer number
+ *
+ * Return: the units value of r
+ */
+
+int print_last_digit(int r)
+{
+	return (r % 10);
+}
