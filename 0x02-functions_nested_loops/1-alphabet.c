2ea0a7a56d9fb37c8f18ee229c924d62c2df3493
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-21 15:23:46 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
