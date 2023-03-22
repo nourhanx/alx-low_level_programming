31145e4253e5f40df37202ba56ea5dd865ccc7db
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-21 17:38:08 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,29 @@ void print_to_98(int n)
 	{
 		while (n < 99)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		while (n > 99)
+		while (n > 97)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 			n--;
 		}
 	}
