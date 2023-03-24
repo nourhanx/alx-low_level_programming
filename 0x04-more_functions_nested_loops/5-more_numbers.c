f333c21828f225c3274414c84feef444846505aa
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-23 17:11:44 -0700

    .

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - 10 times the numbers, from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int count = 0;
+	int i;
+
+	while (count < 20)
+	{
+		for (i = 48; i < 63; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+}
