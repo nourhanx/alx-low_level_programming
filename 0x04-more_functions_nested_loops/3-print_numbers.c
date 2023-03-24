bbb13babbad4777eb21514f10faa6a2a71fcb842
nourhan <nourhan_ashmawy@yahoo.com>
2023-03-23 17:00:36 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+}
