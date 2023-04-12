60563e9490543ce751b07356b8f6e8eff8bc5dd3
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-12 16:51:16 -0700

    .

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,10 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		arr[i] = malloc(sizeof(int) * width);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width ; j++)
