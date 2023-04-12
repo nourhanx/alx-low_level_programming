18ad11b431a650f48ab5535cb5144d1aed3baa89
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-12 15:44:16 -0700

    .

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a new string which is a dubplicate
+ * of given string
+ *
+ * @str: string to be copied
+ *
+ * Return: pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	int length = 0;
+	int i;
+	char *new;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new = malloc(sizeof(char) * length);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		new[i] = str[i];
+	}
+	return (str);
+}
