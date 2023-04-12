d87397ccd8eb3494cb353fca2bef64c80c9953ea
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-12 15:22:57 -0700

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
-	int i;
 
 	for (i = 0 ; i < size ; i++)
 	{
 		arr[i] = c;
 	}
+	return (arr);
 }
