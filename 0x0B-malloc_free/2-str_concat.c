d8ede841a0dcfd421538928a1cdceadc8e9c1f94
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-12 16:27:52 -0700

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,15 +19,28 @@ char *str_concat(char *s1, char *s2)
 	int count = 0;
 	int full_length;
 
-	len1 = get_length(s1);
-	len2 = get_length(s2);
+	if (s1)
+	{
+		len1 = get_length(s1);
+	}
+	else
+	{
+		len1 = 1;
+		s1 = "";
+	}
+	if (s2)
+	{
+		len2 = get_length(s2);
+	}
+	else
+	{
+		len2 = 1;
+		s2 = "";
+	}
 	full_length = len1 + len2;
-
 	new  = malloc((sizeof(char) * full_length) + 1);
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0 ; i < len1 ; i++)
 	{
 		new[i] = s1[i];
@@ -53,7 +66,7 @@ int get_length(char *str)
 {
 	int length = 0;
 
-	while (str[length] && str)
+	while (str[length] != '\0')
 	{
 		length++;
 	}
