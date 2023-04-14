2f56592f9c61ee7020a2aff4f4218ff19e1ed45c
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-13 19:47:28 -0700

    .

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,9 +13,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *a;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
 	a = calloc(nmemb , size);
 	if (a == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		a[i] = 0;
+
 	return (a);
 }
