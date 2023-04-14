99c46f3ab74cce05569351d9ec96dead69c65a30
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-13 19:24:03 -0700

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		while (s2[len2] != '\0')
 			len2++;
-		if (len2 > n)
+		if (len2 >= n)
 			len2 = n;
 	}
 	full_length = len1 + len2;
