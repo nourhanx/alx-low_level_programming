583e1a17c0c85586c95115f9bb71cabf075d2593
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-13 17:56:44 -0700

    .

---
+++ 0x0C-more_malloc_fre/0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc and handles malloc fail
+ *
+ * @b: size of memory to be allocated (int)
+ *
+ * Return: pointer to allocated memory in case of success, otherwise trtutns (98)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+	a = malloc(b);
+	if (a == NULL)
+		return ((void *)98);
+	return (a);
+}
