c89b59325ee7d523231e8beb6983e146f2e8b7c3
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-19 14:47:02 -0700

    .

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
