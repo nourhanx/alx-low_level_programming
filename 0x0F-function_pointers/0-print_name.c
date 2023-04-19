9351249bc8af9978f2c511c43209d561195a8f00
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-19 08:26:06 -0700

    .

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name using a certain format
+ *
+ * @name: a character array of name to be printed
+ * @f: a pointer to function holding the printing format
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
