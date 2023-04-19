6e2811a95b65564dbeb8772b5bb3f67df9bccc2d
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-19 14:33:59 -0700

    .

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0 ; i < size ; i++)
 	{
-		if (cmp(array + i * sizeof(int)))
+		if (cmp((int)(array + i * sizeof(int)))
 			return (i);
 	}
 	return (-1);
