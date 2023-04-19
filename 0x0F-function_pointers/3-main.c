c8de63f6bee32d9e8166fa9bd13222e5aa63ced0
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-19 16:22:02 -0700

    .

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,8 +13,17 @@ int main(int argc, char **argv)
 {
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
-	int (*f)(int, int) = get_op_func(argv[2]);
+	char *op = argv[2];
+	int (*f)(int, int) = get_op_func(op);
 	(void)argc;
+
+	if (op[3] != '\0')
+		printf("Error");
+
+	else if (op[2] != '\0')
+		*op = op[1];
+
+	f = get_op_func(op);
 
 	printf("%d", f(num1, num2));
 	return (0);
