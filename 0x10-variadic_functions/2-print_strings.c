4d7a5d7e247ef7bf2acd62fdf1695be0a0f27f98
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-20 16:32:35 -0700

    .

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,12 +15,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n == 0)
-	{
-		printf("\n");
-		return;
-	}
-
 	for (i = 0; i < n - 1; i++)
 	{
 		s = va_arg(list, char*);
