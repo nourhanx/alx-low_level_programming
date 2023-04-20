4bc3b2759cc974462e49c8cc0c963c45cb6f1044
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-19 17:24:28 -0700

    .

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - prints its own opcodes
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	return (0);
+}
