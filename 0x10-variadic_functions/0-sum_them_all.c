85b87e23ab569ed86092e7615c2cfba30f6bc420
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-20 15:38:04 -0700

    .

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * sum_them_all - sums all arguments
+ *
+ * @n: first integer arg
+ *
+ * Return: sum of all arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int i;
+	int sum = 0;
+	va_start(list, n);
+	
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,9 @@
+This directory contains solutions to problems about variadic functions.
+
+0- function that returns the sum of all its parameters.
+
+1- function that prints numbers, followed by a new line.
+
+2- prints strings, followed by a new line.
+
+3- function that prints anything.
