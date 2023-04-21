26db0f07e1aed5a10bb312584de6fb187f1b6f9b
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-20 17:06:46 -0700

    .

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,3 +5,41 @@
  *
  * @format - a list of types of arguments passed to the function
  */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	char *separator = ", ";
+
+	va_start(list, format);
+
+
+	while (format[i])
+	{
+		if (!format[i + 1])
+			separator = "";
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c, ", va_arg(list, int));
+				i++;
+				break;
+			case 'i':
+				printf("%d, ", va_arg(list, int));
+				i++;
+				break;
+			case 'f':
+				printf("%f, ", va_arg(list, double));
+				i++;
+				break;
+			case 's':
+				printf("%s, ", va_arg(list, char*));
+				i++;
+				break;
+			default:
+				i++;
+		}
+	}
+	printf("\n");
+}
