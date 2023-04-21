7d3bd9027317b1c94324bc187efa4e41392dc16a
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-20 17:33:25 -0700

    .

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,38 +10,42 @@ void print_all(const char * const format, ...)
 {
 	va_list list;
 	int i = 0;
-	char *separator = ", ";
-	char *s;
+	char *separator = "", *s;
 
 	va_start(list, format);
 
-	while (format[i])
+	if (format)
 	{
-		if (!format[i + 1])
-			separator = "\n";
-		switch (format[i])
+		while (format[i])
 		{
-		case 'c':
-			printf("%c%s", va_arg(list, int), separator);
-			i++;
-			break;
-		case 'i':
-			printf("%d%s", va_arg(list, int), separator);
-			i++;
-			break;
-		case 'f':
-			printf("%f%s", va_arg(list, double), separator);
-			i++;
-			break;
-		case 's':
-			s = va_arg(list, char*);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s%s", s, separator);
-			i++;
-			break;
-		default:
-			i++;
+			if (!format[i + 1])
+				separator = "\n";
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", separator, va_arg(list, int));
+				i++;
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(list, int));
+				i++;
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(list, double));
+				i++;
+				break;
+			case 's':
+				s = va_arg(list, char*);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				i++;
+				break;
+			default:
+				i++;
+			}
+			separator = ", ";
 		}
+		printf("\n");
 	}
 }
