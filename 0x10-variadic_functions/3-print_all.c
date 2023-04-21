d2bf85df02a4b535d56ada35b10676918c086829
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-20 17:22:18 -0700

    .

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,36 +15,33 @@ void print_all(const char * const format, ...)
 
 	va_start(list, format);
 
-	while (format)
+	while (format[i])
 	{
-		while (format[i])
+		if (!format[i + 1])
+			separator = "\n";
+		switch (format[i])
 		{
-			if (!format[i + 1])
-				separator = "\n";
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c%s", va_arg(list, int), separator);
-				i++;
-				break;
-			case 'i':
-				printf("%d%s", va_arg(list, int), separator);
-				i++;
-				break;
-			case 'f':
-				printf("%f%s", va_arg(list, double), separator);
-				i++;
-				break;
-			case 's':
-				s = va_arg(list, char*);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s%s", s, separator);
-				i++;
-				break;
-			default:
-				i++;
-			}
+		case 'c':
+			printf("%c%s", va_arg(list, int), separator);
+			i++;
+			break;
+		case 'i':
+			printf("%d%s", va_arg(list, int), separator);
+			i++;
+			break;
+		case 'f':
+			printf("%f%s", va_arg(list, double), separator);
+			i++;
+			break;
+		case 's':
+			s = va_arg(list, char*);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s%s", s, separator);
+			i++;
+			break;
+		default:
+			i++;
 		}
 	}
 }
