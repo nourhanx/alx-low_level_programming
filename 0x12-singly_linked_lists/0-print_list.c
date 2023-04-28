fd21a0b7f69266c9e4dd0c6af076a3635242f544
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-28 00:32:59 -0700

    feat: add files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ *
+ * @h: list to be printed
+ *
+ * Return: size of printed list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (!h)
+		return 0;
+
+	while (h)
+	{
+		if ((*h).str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] ", (*h).len);
+			printf("%s\n", (*h).str);
+		}
+		h = (*h).next;
+		count++;
+	}
+	return (count);
+}
