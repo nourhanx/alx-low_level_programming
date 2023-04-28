8f3ecbda1f7de126802aa5eb8c012a2621cb8f8e
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-28 02:05:22 -0700

    feat: add file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: pointer to head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
