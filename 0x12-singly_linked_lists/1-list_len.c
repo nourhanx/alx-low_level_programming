99ff6362fc2b8a482f68635062f3aceef9749789
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-28 00:46:49 -0700

    feat: fixes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,12 +3,12 @@
 /**
  * list_len - returns number of elements in a linked list_t list
  *
- * h: a linked list_t list
+ * @h: a linked list_t list
  *
  * Return: length of elements in h
  */
 
-size_t list_len(const list_h *h)
+size_t list_len(const list_t *h)
 {
 	size_t len = 0;
 
