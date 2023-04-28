23ac4fd13b99c048c30df25b378e9da9d1427a54
nourhan <nourhan_ashmawy@yahoo.com>
2023-04-28 02:13:38 -0700

    feat: fixes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * before - is executed before main
  */
 
-void __attribute__ ((constructoe)) first()
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
