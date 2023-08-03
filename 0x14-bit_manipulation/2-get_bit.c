f95f82e5699ca6ccd90fc159c2f27ddaa8971243
nourhan <nourhan_ashmawy@yahoo.com>
2023-08-03 07:00:25 -0700

    mod file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int mask;
 
-	if (index < 0 || index >= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	mask = 1 << index;
