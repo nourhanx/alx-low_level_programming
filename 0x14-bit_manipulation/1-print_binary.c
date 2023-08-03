24f6dc77a949828523424174e889718ff277f4e3
nourhan <nourhan_ashmawy@yahoo.com>
2023-08-03 06:23:25 -0700

    add file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+/**
+ * print_binary - prints binary representation of a number
+ *
+ * @n: unsigned long int to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bits, highestBit;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	bits = sizeof(unsigned long int) * 8;
+	highestBit = 1 << (bits - 1);
+
+	while (!highestBit && n)
+		highestBit >>= 1;
+
+	while (highestBit)
+	{
+		_putchar(n & highestBit);
+		highestBit >>= 1;
+	}
+}
